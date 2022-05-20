c99d142ac35c19576febcb4dec22ced3eea51bc5
eric2788 <tsukiko852@gmail.com>
2022-05-20 23:19:57 +0800

    fix didn't register SQLInstallation

--- eldependenci-sql-plugin/src/main/java/chu77/eldependenci/sql/ServiceCollectionBinder.java
+++ eldependenci-sql-plugin/src/main/java/chu77/eldependenci/sql/ServiceCollectionBinder.java
@@ -1,12 +1,14 @@
 package chu77.eldependenci.sql;
 
 import chu77.eldependenci.sql.config.Dbconfig;
+import chu77.eldependenci.sql.jpa.JpaRepositoryModule;
 import chu77.eldependenci.sql.manager.JpaFactoryManager;
 import chu77.eldependenci.sql.manager.SQLDataSourceManager;
 import chu77.eldependenci.sql.manager.datasource.CustomDataSource;
 import chu77.eldependenci.sql.manager.datasource.ELDDataSourceFactory;
 import chu77.eldependenci.sql.manager.datasource.MySQLDataSource;
 import chu77.eldependenci.sql.manager.datasource.SQLiteDataSource;
+import com.ericlam.mc.eld.AddonInstallation;
 import com.ericlam.mc.eld.ServiceCollection;
 
 import java.util.Map;
@@ -24,5 +26,13 @@ public class ServiceCollectionBinder {
 
         serviceCollection.bindService(SQLService.class, SQLDataSourceManager.class);
         serviceCollection.addSingleton(JpaFactoryManager.class);
+
+        var sqlInstaller = new ELDSQLInstallation();
+
+        AddonInstallation addon = serviceCollection.getInstallation(AddonInstallation.class);
+
+        addon.customInstallation(SQLInstallation.class, sqlInstaller);
+        addon.installModule(new JpaRepositoryModule(sqlInstaller));
+
     }
 }
