fd567b4529eddfc2ef5f2aa07544d61df16b0c6c
eric2788 <tsukiko852@gmail.com>
2022-05-20 23:44:29 +0800

    update for v2.0.3

--- eldependenci-sql-plugin/src/main/java/chu77/eldependenci/sql/bukkit/SQLAddonRegistry.java
+++ eldependenci-sql-plugin/src/main/java/chu77/eldependenci/sql/bukkit/SQLAddonRegistry.java
@@ -1,15 +1,16 @@
 package chu77.eldependenci.sql.bukkit;
 
 import com.ericlam.mc.eld.BukkitRegistry;
-import com.ericlam.mc.eld.components.CommandNode;
+import com.ericlam.mc.eld.components.BukkitCommand;
 import com.ericlam.mc.eld.registration.CommandRegistry;
 import com.ericlam.mc.eld.registration.ListenerRegistry;
 import org.bukkit.event.Listener;
 
 public class SQLAddonRegistry implements BukkitRegistry {
 
+
     @Override
-    public void registerCommand(CommandRegistry<CommandNode> commandRegistry) {
+    public void registerCommand(CommandRegistry<BukkitCommand> commandRegistry) {
 
     }
 
--- eldependenci-sql-plugin/src/main/java/chu77/eldependenci/sql/bungee/SQLAddonRegistry.java
+++ eldependenci-sql-plugin/src/main/java/chu77/eldependenci/sql/bungee/SQLAddonRegistry.java
@@ -1,14 +1,16 @@
 package chu77.eldependenci.sql.bungee;
 
 import com.ericlam.mc.eld.BungeeRegistry;
-import com.ericlam.mc.eld.components.CommandNode;
+import com.ericlam.mc.eld.components.BungeeCommand;
 import com.ericlam.mc.eld.registration.CommandRegistry;
 import com.ericlam.mc.eld.registration.ListenerRegistry;
 import net.md_5.bungee.api.plugin.Listener;
 
 public class SQLAddonRegistry implements BungeeRegistry {
+
+
     @Override
-    public void registerCommand(CommandRegistry<CommandNode> commandRegistry) {
+    public void registerCommand(CommandRegistry<BungeeCommand> commandRegistry) {
 
     }
 
